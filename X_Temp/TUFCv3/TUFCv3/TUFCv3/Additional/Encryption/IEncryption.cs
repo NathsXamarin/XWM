759dfa7397dadc163da443f1db829dd1d46dafcb
Nath <NathsXamarin@hotmailcom>
2022-10-25 09:27:28 +1300

    Removed TUFCv3.UnitTest
    Some of the XUnit NuGet packages have changed name to 'More.XUnit.name'
    
    I discovered this after trying to install the original versions.
    
    To keep the app tidy I removed the original project 'TUFCv3.UnitTest' and start again.

--- X_Temp/TUFCv3/TUFCv3/TUFCv3/Additional/Encryption/IEncryption.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TUFCv3.Additional.Encryption
-{
-    public interface IEncryption
-    {
-        string DecryptText(string cipherText);      // Decrypt an encryted string
-        string EncryptText(string plainText);       // Encrypt plain text
-    }
-}
